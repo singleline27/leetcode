ab5061f89e5e0d1b25858eb6a59210dbc6df523a
Chang Liu <fifa_1949@hotmail.com>
2013-09-04 21:22:26 -0700

    distinctive sequence - dp solution
    500ms
    548ms

--- distinctSubsequences.java
+++ distinctSubsequences.java
@@ -5,27 +5,23 @@ public class Solution {
             
         if(T == null || S == null) return 0;
         if(T.equals("")) return 1;
+
+        int m = S.length();
+        int n = T.length();
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
         
-        return findSub(S, T, 0, 0);
-    }
-    
-    private int findSub(String S, String T, int i, int j) {
-    
-        if(j == T.length()) return 1;
-        
-        if(i >= S.length() || j > T.length()) return 0;
-        
-        char c = T.charAt(j);
-        int curr = S.indexOf(c, i);
-        int sum = 0;
-        
-        while(curr >= 0) {
-            
-            sum += findSub(S, T, curr + 1, j + 1);
-            
-            curr = S.indexOf(c, curr + 1);
+        for(int i = 1; i <= m; ++i) {
+            for(int j = n; j > 0; --j) {
+                if(S.charAt(i-1) == T.charAt(j-1) && dp[j - 1] > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
         }
         
-        return sum;
+        return dp[n];
+            
     }
+    
 }
