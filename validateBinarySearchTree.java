09d112abc99f5f46f60b3a895677b0d08e9ac194
Chang Liu <fifa_1949@hotmail.com>
2013-09-05 01:47:28 -0700

    validate binary search tree - non recursive
    544ms
    624ms

--- validateBinarySearchTree.java
+++ validateBinarySearchTree.java
@@ -31,3 +31,48 @@ public class Solution {
         return Math.max(Math.max(root.val, maxValue(root.left)), maxValue(root.right));
     }
 }
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(root == null) return true;
+        
+        Stack<TreeNode> stack = new Stack();
+        boolean done = false;
+        TreeNode curr = root;
+        
+        int last = Integer.MIN_VALUE;
+        while(!done) {
+            
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            
+            if(stack.empty()) {
+                done = true;
+            } else {
+                TreeNode node = stack.pop();
+                if(node.val <= last) {
+                    return false;
+                }
+                
+                last = node.val;
+                curr = node.right;
+            }
+        }
+        
+        return true;
+    }
+    
+}
