79334c59194c1986d2487b177263f25cf7e36579
Chang Liu <fifa_1949@hotmail.com>
2013-09-27 13:30:04 -0700

    length of last word

--- lengthOfLastWord.java
+++ lengthOfLastWord.java
@@ -18,3 +18,15 @@ public class Solution {
         return count;
     }
 }
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        String[] sec = s.split(" ");
+        for(int i = sec.length -1; i >= 0; --i) {
+            if(sec[i].length() > 0) return sec[i].length();
+        }
+        return 0;
+    }
+}
