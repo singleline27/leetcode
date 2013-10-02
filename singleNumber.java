031a02bf6bb24bc0e8309e26ce4b8cb5370ddebd
Chang Liu <fifa_1949@hotmail.com>
2013-10-02 16:20:41 -0700

    single number

---
+++ singleNumber.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int singleNumber(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int result = A[0];
+        for(int i = 1; i < A.length; ++i) {
+            result ^= A[i];
+        }
+        return result;
+    }
+}
